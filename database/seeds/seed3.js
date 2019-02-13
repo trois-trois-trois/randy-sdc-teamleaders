48a89fc9bbf3d0ab64a6b09198e06ac53dee8f84
Randy <rlthomasccv@gmail.com>
2019-02-12 18:30:08 -0800

    FIXED NPM ERROR SEED3

--- database/seeds/seed3.js
+++ database/seeds/seed3.js
@@ -1,11 +1,13 @@
-const db = require('../config');
+// const db = require('../config');
 
-const copy = "COPY espn.stats FROM 'users/rlthomasmaster/seedscript.csv' WITH DELIMITER='|' AND HEADER=TRUE";
+// const copy = "COPY espn.stats FROM 'users/rlthomasmaster/seedscript.csv' WITH DELIMITER='|' AND HEADER=TRUE";
 
-db.execute(copy, [], (err, res) => {
-    if (err) console.log('ERROR: ', err);
-    else {
-      console.log('ESPN stats data Copied :)');
-    }
-  });
+// db.execute(copy, [], (err, res) => {
+//     if (err) console.log('ERROR: ', err);
+//     else {
+//       console.log('ESPN stats data Copied :)');
+//     }
+//   });
 
+USE espn;
+COPY espn.stats FROM '/users/rlthomasmaster/github/seedscript.csv' WITH DELIMITER='|' AND HEADER=TRUE;
