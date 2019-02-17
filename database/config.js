3c68642b18cc6bf0aa133cdde3641d0613297607
Randy <rlthomasccv@gmail.com>
2019-02-16 21:55:02 -0800

    10 M records to EC2 in 1:30:00

--- database/config.js
+++ database/config.js
@@ -1,9 +1,11 @@
+const pass = require('../sensitive')
+
 const knex = require('knex')({
     client: 'pg',
     connection: {
-      host: '127.0.0.1',
+      host: 'ec2-18-223-44-22.us-east-2.compute.amazonaws.com',
       user: 'me',
-      password: '',
+      password: pass.password,
       database: 'espn',
     },
   });
